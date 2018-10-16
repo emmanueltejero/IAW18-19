10fd245f95729f623c63b67d8280331cdc079766
Alumno <Alumno@AULA29-13>
2018-10-16 13:56:17 +0200

    16-10

---
+++ ejerciciosbucles/src/es/cj/datos/explicacion.java
@@ -0,0 +1,34 @@
+package es.cj.datos;
+
+import java.util.Scanner;
+
+public class explicacion {
+	
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// do while
+		int numero;
+		do {
+			 System.out.println("Nº: ");
+			 numero = sc.nextInt();
+		} while (numero < 0);
+		
+		
+		//while
+	
+		while (numero >= 0) {
+			System.out.println(numero);
+			numero --;
+		}
+		
+		//for
+
+		for (int i = numero; i >= 0; i--) {
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
